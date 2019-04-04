462afb3f1b6d4ab76e7734cdf6967d7eacf452f9
Thomas Waters <twaters117@gmail.com>
2019-04-04 16:34:35 -0400

    Circular Patchwork

--- patchwork.js
+++ patchwork.js
@@ -1,45 +1,71 @@
 import { PaperSize, Orientation } from 'penplot';
+import { randomFloat } from 'penplot/util/random';
 import { polylinesToSVG } from 'penplot/util/svg';
 import { clipPolylinesToBox } from 'penplot/util/geom';
+import clustering from 'density-clustering';
+import convexHull from 'convex-hull';
+import array from 'new-array';
 
 export const orientation = Orientation.LANDSCAPE;
 export const dimensions = PaperSize.LETTER;
 
 export default function createPlot (context, dimensions) {
   const [ width, height ] = dimensions;
-  let lines = [];
+  const center            = [ width / 2, height / 2 ];
+  const margin            = 1.5;
+  const max_radius        = Math.min(center[0], center[1]) - margin;
+  const cluster_count     = 5;
+  const fps               = 30;
+  const lines             = [];
+  setInterval(update, 1000 / fps);
 
-  // Draw some circles expanding outward
-  const steps = 5;
-  const count = 20;
-  const spacing = 1;
-  const radius = 2;
-  for (let j = 0; j < count; j++) {
-    const r = radius + j * spacing;
-    const circle = [];
-    for (let i = 0; i < steps; i++) {
-      const t = i / Math.max(1, steps - 1);
-      const angle = Math.PI * 2 * t;
-      circle.push([
-        width / 2 + Math.cos(angle) * r,
-        height / 2 + Math.sin(angle) * r
-      ]);
-    }
-    lines.push(circle);
-  }
+  // Generate the random points
+  const point_count = 50000; // Larger number will make more defined results
+  let points  = array(point_count).map(() => {
+    return randomInCircle(center, max_radius);
+  });
+
 
   // Clip all the lines to a margin
-  const margin = 1.5;
-  const box = [ margin, margin, width - margin, height - margin ];
-  lines = clipPolylinesToBox(lines, box);
+  // const box = [ margin, margin, width - margin, height - margin ];
+  // lines = clipPolylinesToBox(lines, box);
 
   return {
     draw,
     print,
     background: 'white',
-    animate: false,
-    clear: true
+    animate: true,
+    // clear: true
   };
+
+  function update() {
+    // The generation algorithm
+    if (points.length <= cluster_count) return;
+
+    const scan = new clustering.KMEANS();
+    const clusters = scan.run(points, cluster_count)
+      .filter(c => c.length >= 3);
+
+    if (clusters.length === 0) return;
+
+    // Sort clusters by density
+    clusters.sort((a, b) => b.length - a.length);
+
+    const cluster = clusters[0];
+    const positions = cluster.map(i => points[i]);
+
+    // Finding the convex hull of the cluster
+    const edges = convexHull(positions);
+    if (edges.length <= 2) return;
+
+    let path = edges.map(c => positions[c[0]]);
+    path.push(path[0]);
+
+    lines.push(path);
+    console.log(path);
+
+    points = points.filter(p => !positions.includes(p));
+  }
 
   function draw () {
     lines.forEach(points => {
@@ -53,5 +79,15 @@ export default function createPlot (context, dimensions) {
     return polylinesToSVG(lines, {
       dimensions
     });
+  }
+
+  // Helper functions
+  function randomInCircle(center, radius) {
+    const r = radius * Math.sqrt(Math.random());
+    const a = randomFloat(0, 2*Math.PI);
+    return [
+      center[0] + r * Math.cos(a),
+      center[1] + r * Math.sin(a)
+    ];
   }
 }
